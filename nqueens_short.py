5d85180c5ca581f2883b62040614b105af9a2a93
Cédric Donner <cedonner@gmail.com>
2025-02-08 07:56:10 +0100

    fix toycsp nqueens example

--- nqueens_short.py
+++ nqueens_short.py
@@ -1,21 +1,25 @@
 from toycsp import ToyCSP, Variable, NotEqual
 
-n = int(input("Taille du problème: "))
+def nqueens_solver(n: int) -> list[list[int]]:
+    csp: ToyCSP = ToyCSP()
+    q: list[Variable] = [csp.add_variable(range(n)) for _ in range(n)]
 
-csp: ToyCSP = ToyCSP()
-q: list[Variable] = [csp.add_variable(range(n)) for _ in range(n)]
+    for i in range(n):
+        for j in range(i + 1, n):
+            csp.post(NotEqual(q[i], q[j], 0))
+            csp.post(NotEqual(q[i], q[j], i - j))
+            csp.post(NotEqual(q[i], q[j], j - i))
 
-for i in range(n):
-    for j in range(i + 1, n):
-        csp.post(NotEqual(q[i], q[j], 0))
-        csp.post(NotEqual(q[i], q[j], i - j))
-        csp.post(NotEqual(q[i], q[j], j - i))
+    @csp.on('solution')
+    def handle_solution(csp, infos):
+        solutions.append(csp.get_solution())
 
-@csp.on('solution')
-def handle_solution(csp, infos):
-    solutions.append(csp.get_solution())
+    solutions = []
+    csp.dfs()
+    
+    return solutions
 
-solutions = []
-csp.dfs()
 
-print(f"{solutions = }")
+if __name__ == '__main__':
+    solutions = nqueens_solver(n=4)
+    print(f"{solutions = }")
